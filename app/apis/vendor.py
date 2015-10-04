cb74ed8032bd408051c32c645e27237e9ff1108e
looklikeapro <jerryzhou00@gmail.com>
2015-10-04 17:25:25 -0400

    init

---
+++ app/apis/vendor.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template import RequestContext, loader
+from django.shortcuts import render
+
+
+class Vendor(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("herro")
+	# def post(self, request, *args, **kwargs):
+	# 	template = loader.get_template('main.html')
+	# 	context = RequestContext(request, {"result": randomize()})
+	# 	return HttpResponse(template.render(context))
+		# def get(self, request, *args, **kwargs):
+		#     return HttpResponse("Hello, World")
+
+# def welcome(request):
+#     return HttpResponse("Hello, World")
