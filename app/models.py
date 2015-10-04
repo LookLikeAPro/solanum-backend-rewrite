be4d440c6c6f0fd17c5322b202e8c31547f943ac
looklikeapro <jerryzhou00@gmail.com>
2015-10-04 18:44:38 -0400

    transfer models

--- app/models.py
+++ app/models.py
@@ -2,6 +2,56 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	username = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
 	password = models.CharField(max_length=200)
-	email = models.CharField(max_length=200)
+	email = models.EmailField(max_length=254)
+	vendors = models.ManyToManyField('Vendor', through='VendorManagement', through_fields=('user', 'vendor'))
+
+class Vendor(models.Model):
+	name = models.CharField(max_length=200)
+	link = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	longitude = models.FloatField()
+	latitude = models.FloatField()
+	address = models.CharField(max_length=200)
+	city = models.CharField(max_length=200)
+	province = models.CharField(max_length=200)
+	area_code = models.CharField(max_length=200)
+	country_code = models.CharField(max_length=200)
+	timezone = models.IntegerField()
+	phone_number = models.CharField(max_length=200)
+	email = models.EmailField(max_length=254)
+	users = models.ManyToManyField('User', through='VendorManagement', through_fields=('vendor', 'user'))
+
+class VendorManagement(models.Model):
+	vendor = models.ForeignKey('Vendor')
+	user = models.ForeignKey('User')
+
+class Product(models.Model):
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	price = models.IntegerField()
+	currency = (
+		('0', 'USD'),
+		('1', 'CAD'),
+		('2', 'EUR'),
+		('3', 'RMB'),
+	)
+
+class Transaction(models.Model):
+	amount = models.IntegerField()
+	date = models.DateTimeField(auto_now_add=True)
+	user = models.ForeignKey('User')
+	vendor = models.ForeignKey('Vendor')
+
+class TransactionItem(models.Model):
+	transation = models.ForeignKey('Transaction')
+	amount = models.IntegerField()
+	product = models.ForeignKey('Product')
+	count = models.IntegerField()
+
+class CartItem(models.Model):
+	user = models.ForeignKey('User')
+	product = models.ForeignKey('Product')
+	count = models.IntegerField()
+
