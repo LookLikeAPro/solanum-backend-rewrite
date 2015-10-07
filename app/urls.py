0301c496cdfe7fdd3fe9b035eee043ac3a5ce258
looklikeapro <jerryzhou00@gmail.com>
2015-10-07 18:46:13 -0400

    add main view template

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from .apis import *
+import views
 
 urlpatterns = [
 	url(r'^api/auth/login', auth.login.as_view()),
 	url(r'^api/auth/logout', auth.logout.as_view()),
 	# url(r'^api/?', auth.as_view()),
+	url(r'^/?', views.app.as_view())
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template import RequestContext, loader
 from django.shortcuts import render
 
-# Create your views here.
+class app(View):
+	def get(self, request, *args, **kwargs):
+		template = loader.get_template('app.html')
+		context = RequestContext(request, {"data": "", "render": ""})
+		return HttpResponse(template.render(context))
